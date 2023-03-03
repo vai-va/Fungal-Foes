f15485a56e2f162ed7c44478bd84e6bb4ae2fa2e
Vaiva Mačiulytė <vaimaciulyte@gmail.com>
2023-03-03 17:08:16 +0200

    Player animations
    Player has animations for moving in 4 directions, added idling animations for 4 directions, added a boundary script for character not to go off screen.

--- Fungal Foes/Assets/PlayerMovement.cs
+++ Fungal Foes/Assets/PlayerMovement.cs
@@ -8,6 +8,11 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     Vector2 move;
     public float moveSpeed;
+    public Animator animator;
+
+    //public Transform interactor;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +25,38 @@ public class PlayerMovement : MonoBehaviour
     {
         move.x = Joystick.Horizontal;
         move.y = Joystick.Vertical;
+
+        animator.SetFloat("Horizontal", move.x);
+        animator.SetFloat("Vertical", move.y);
+        animator.SetFloat("Speed", move.sqrMagnitude);
+
+        if (move.x != 0 || move.y != 0)
+        {
+            animator.SetFloat("LastHorizontal", Joystick.Horizontal);
+            animator.SetFloat("LastVertical", Joystick.Vertical);
+        }
+
+        //if (move.x > 0)
+        //{
+        //    interactor.localRotation = Quaternion.Euler(0, 0, 90);
+        //}
+        //if (move.x < 0)
+        //{
+        //    interactor.localRotation = Quaternion.Euler(0, 0, -90);
+        //}
+        //if (move.y > 0)
+        //{
+        //    interactor.localRotation = Quaternion.Euler(0, 0, 180);
+        //}
+        //if (move.y < 0)
+        //{
+        //    interactor.localRotation = Quaternion.Euler(0, 0, 0);
+        //}
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + move * moveSpeed * Time.deltaTime);
+
     }
 }
