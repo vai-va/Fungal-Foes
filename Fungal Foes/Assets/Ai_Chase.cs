be45e4fd6ad7b68e08f48c531f0acd4f1bca8518
Valento135 <125828881+Valento135@users.noreply.github.com>
2023-03-08 23:05:10 +0200

    Enemy chasing player

---
+++ Fungal Foes/Assets/Ai_Chase.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ai_Chase : MonoBehaviour
+{
+    public GameObject Player;
+    public float speed;
+    private Animator anim;
+
+    private float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Player = GameObject.FindWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(transform.position,Player.transform.position);
+        Vector2 direction = Player.transform.position - transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, Player.transform.position,speed * Time.deltaTime);
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        direction.Normalize();
+        anim.SetFloat("x", direction.x);
+        anim.SetFloat("y", direction.y);
+
+
+    }
+}
