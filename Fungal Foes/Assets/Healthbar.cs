61cec1df13348f87850ef25377a8c6eeb966acf6
Valento135 <125828881+Valento135@users.noreply.github.com>
2023-03-24 15:26:08 +0200

    Added health bar

---
+++ Fungal Foes/Assets/Healthbar.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxhealth(int maxHealth)
+    {
+        slider.maxValue = maxHealth;
+        slider.value = maxHealth;
+    }
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+}
