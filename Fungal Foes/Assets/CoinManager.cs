2fad79dbf8d979af3f296b6d0eeb88e17a7ed011
rokenga <robertaruka@gmail.com>
2023-04-12 02:35:04 +0300

    Store thingies

--- Fungal Foes/Assets/CoinManager.cs
+++ Fungal Foes/Assets/CoinManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CoinManager : MonoBehaviour
 {
@@ -16,11 +17,24 @@ public class CoinManager : MonoBehaviour
         {
             instance = this;
         }
+
+        if (SceneManager.GetActiveScene().name == "Store" || SceneManager.GetActiveScene().name == "Level1")
+        {
+            if (PlayerPrefs.HasKey("CoinScore"))
+            {
+                score = PlayerPrefs.GetInt("CoinScore");
+                text.text = "X" + score.ToString();
+            }
+        }
+
     }
 
     public void ChangeScore(int coinValue)
     {
         score += coinValue;
         text.text = "X" + score.ToString();
+        PlayerPrefs.SetInt("CoinScore", score);
+        PlayerPrefs.Save();
+
     }
 }
