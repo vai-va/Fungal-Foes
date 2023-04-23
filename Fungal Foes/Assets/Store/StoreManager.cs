818a3a6d52c8a4a769535f9d10b7e4629deea23e
rokenga <robertaruka@gmail.com>
2023-04-24 01:47:16 +0300

    some more

--- Fungal Foes/Assets/Store/StoreManager.cs
+++ Fungal Foes/Assets/Store/StoreManager.cs
@@ -15,6 +15,11 @@ public class StoreManager : MonoBehaviour
 
     private void Start()
     {
+        PlayerPrefs.DeleteKey("CurrentItem");
+        for (int i = 0; i < storeItem.Length; i++)
+        {
+            PlayerPrefs.DeleteKey(storeItem[i].title);
+        }
         coinManager = CoinManager.instance;
         coins = coinManager.score;
         LoadPanels();
@@ -28,6 +33,12 @@ public class StoreManager : MonoBehaviour
             storePanels[i].titleTXT.text = storeItem[i].title;
             storePanels[i].costTXT.text = storeItem[i].baseCost.ToString() + " COINS";
             storePanels[i].image.sprite = storeItem[i].image;
+
+            //Check if the item is bought and update the button text accordingly
+            if (PlayerPrefs.HasKey(storeItem[i].title) && PlayerPrefs.GetInt(storeItem[i].title) == 1)
+            {
+                purchaseButton[i].GetComponentInChildren<TMP_Text>().text = "USE";
+            }
         }
     }
 
@@ -39,16 +50,27 @@ public class StoreManager : MonoBehaviour
             return;
         }
 
-        // Subtract the cost of the item from the coins
-        coins -= storeItem[itemIndex].baseCost;
+        // Check if the item is already purchased
+        if (PlayerPrefs.HasKey(storeItem[itemIndex].title) && PlayerPrefs.GetInt(storeItem[itemIndex].title) == 1)
+        {
+            // If the item is already purchased, set it as the current item and update the UI
+            UseItem(itemIndex);
+        }
+        else
+        {
+            // Subtract the cost of the item from the coins
+            coins -= storeItem[itemIndex].baseCost;
 
-        // Update the coin value in all scenes where it is visible
-        CoinManager.instance.ChangeScore(-storeItem[itemIndex].baseCost);
-        CheckPurchase();
+            // Update the coin value in all scenes where it is visible
+            CoinManager.instance.ChangeScore(-storeItem[itemIndex].baseCost);
 
-        // TODO: Add code to give the player the item they purchased
+            // Save the purchased item
+            PlayerPrefs.SetInt(storeItem[itemIndex].title, 1);
+            PlayerPrefs.Save();
+
+            CheckPurchase();
+        }
     }
-
 
     public void CheckPurchase()
     {
@@ -59,6 +81,25 @@ public class StoreManager : MonoBehaviour
                 if (coins >= storeItem[i].baseCost)
                 {
                     purchaseButton[i].interactable = true;
+
+                    if (PlayerPrefs.HasKey(storeItem[i].title) && PlayerPrefs.GetInt(storeItem[i].title) == 1)
+                    {
+                        if (PlayerPrefs.GetString("CurrentItem") == storeItem[i].title)
+                        {
+                            purchaseButton[i].GetComponentInChildren<TMP_Text>().text = "USING";
+                            purchaseButton[i].interactable = false;
+                        }
+                        else
+                        {
+                            purchaseButton[i].GetComponentInChildren<TMP_Text>().text = "USE";
+                            purchaseButton[i].interactable = true;
+                        }
+                    }
+                    else
+                    {
+                        purchaseButton[i].GetComponentInChildren<TMP_Text>().text = "BUY";
+                        purchaseButton[i].interactable = true;
+                    }
                 }
                 else
                 {
@@ -68,5 +109,43 @@ public class StoreManager : MonoBehaviour
         }
     }
 
+    public void UseItem(int itemIndex)
+    {
+        // Set the player's current item
+        PlayerPrefs.SetString("CurrentItem", storeItem[itemIndex].title);
+        PlayerPrefs.Save();
 
+        // Update the button text for all items
+        for (int i = 0; i < purchaseButton.Length; i++)
+        {
+            if (i == itemIndex)
+            {
+                purchaseButton[i].GetComponentInChildren<TMP_Text>().text = "USING";
+                purchaseButton[i].interactable = false;
+            }
+            else
+            {
+                if (PlayerPrefs.HasKey(storeItem[i].title) && PlayerPrefs.GetInt(storeItem[i].title) == 1)
+                {
+                    purchaseButton[i].GetComponentInChildren<TMP_Text>().text = "USE";
+                    purchaseButton[i].interactable = true;
+                }
+                else
+                {
+                    purchaseButton[i].GetComponentInChildren<TMP_Text>().text = "BUY";
+                    if (coins >= storeItem[i].baseCost)
+                    {
+                        purchaseButton[i].interactable = true;
+                    }
+                    else
+                    {
+                        purchaseButton[i].interactable = false;
+                    }
+                }
+            }
+        }
+    }
 }
+
+
+
